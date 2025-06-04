24188991795cc8725dfc7454dad653fd9a12a1f3
zentyk <zentyk@protonmail.com>
2025-06-03 22:14:28 -0600

    Finished recaptcha with button

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const whatsappButton = document.getElementById('buttonx');
+if (whatsappButton) {
+  whatsappButton.addEventListener('click', () => {
+    window.open('https://wa.me/+525538903746', '_blank');
+  });
+}
+
+const closeButton = document.getElementById('close-popup');
+if (closeButton) {
+  closeButton.addEventListener('click', () => {
+    const modal = document.getElementById('popup');
+    if (modal) {
+      modal.style.display = 'none';
+    }
+  });
+}
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
